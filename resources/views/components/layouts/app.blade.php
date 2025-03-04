6eccff1a9f4f177e6c30237c76e451ccae8855ff
gtarcea <gtarcea@umich.edu>
2025-03-04 16:10:30 -0500

    Add font-awesome 6

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>{{ $title ?? 'Page Title' }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet"/>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/sass/fa/fontawesome.scss', 'resources/sass/fa/solid.scss', 'resources/js/app.js'])
 </head>
 <body class="container-fluid">
 {{ $slot }}
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/fa/fontawesome.scss',
+                'resources/sass/fa/solid.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
